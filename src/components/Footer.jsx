0196cb81655d497c807ac164c174c17cca6f4276
Vivek <vivekkalp@gmail.com>
2024-09-21 23:46:01 +0530

    name changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
 
       <div className='footer-container'>
         <p>
-          © 2023 <strong>Vivek Kalpavruksha</strong>. All rights reserved.
+          © 2023 <strong>Vivek Kalpavruksh</strong>. All rights reserved.
         </p>
 
         <div className='flex gap-3 justify-center items-center'>
